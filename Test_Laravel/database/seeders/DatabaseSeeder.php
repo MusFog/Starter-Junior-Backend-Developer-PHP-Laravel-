d344d29d439c7c8e5383a88f4bd72afd39fc58d8
MusFog <maximkhutoryansky@gmail.com>
2025-04-02 10:59:16 +0300

    .

--- Test_Laravel/database/seeders/DatabaseSeeder.php
+++ Test_Laravel/database/seeders/DatabaseSeeder.php
@@ -28,48 +28,51 @@ class DatabaseSeeder extends Seeder
 
 
 
-            $supervisors = [];
-            $ns = config('factory.numberSpecimens');
-            $mel = config('factory.max_employee_level');
+//            $supervisors = [];
+//            $ns = config('factory.numberSpecimens');
+//            $mel = config('factory.max_employee_level');
+//
+//            for ($i = 0; $i < $ns; $i++) {
+//                $this->command->info('Seeding took: ' . $i . ' count.');
+//                $users = User::factory(rand(1, 3))->create();
+//
+//                $position = Position::factory()->create([
+//                    'admin_created_id' => $users[0]->id,
+//                    'admin_updated_id' => $users[1]->id ?? $users[0]->id,
+//                ]);
+//
+//                $position->admins()->attach([
+//                    $users[0]->id,
+//                    $users[1]->id ?? $users[0]->id,
+//                ]);
+//
+//                $block = $i % $mel;
+//
+//                $supervisor = $block > 0 && isset($supervisors[$block - 1])
+//                    ? $supervisors[$block - 1]
+//                    : null;
+//
+//                $employee = Employee::factory()->create([
+//                    'position_id' => $position->id,
+//                    'position_name' => $position->name,
+//                    'admin_created_id' => $users[0]->id,
+//                    'admin_updated_id' => $users[1]->id ?? $users[0]->id,
+//                    'level' => 4 - $block,
+//                    'supervisor_id' => $supervisor?->id,
+//                    'supervisor_name' => $supervisor?->employee_name,
+//                ]);
+//
+//                $supervisors[$block] = $employee;
+//            }
 
-            for ($i = 0; $i < $ns; $i++) {
-                $this->command->info('Seeding took: ' . $i . ' count.');
-                $users = User::factory(rand(1, 3))->create();
+//            $admin = User::factory()->create([
+//                'email' => 'admin@gmail.com',
+//                'name' => 'Super Admin',
+//                'password' => bcrypt('admin'),
+//            ]);
 
-                $position = Position::factory()->create([
-                    'admin_created_id' => $users[0]->id,
-                    'admin_updated_id' => $users[1]->id ?? $users[0]->id,
-                ]);
+            $admin = User::where('email', 'admin@gmail.com')->first();
 
-                $position->admins()->attach([
-                    $users[0]->id,
-                    $users[1]->id ?? $users[0]->id,
-                ]);
-
-                $block = $i % $mel;
-
-                $supervisor = $block > 0 && isset($supervisors[$block - 1])
-                    ? $supervisors[$block - 1]
-                    : null;
-
-                $employee = Employee::factory()->create([
-                    'position_id' => $position->id,
-                    'position_name' => $position->name,
-                    'admin_created_id' => $users[0]->id,
-                    'admin_updated_id' => $users[1]->id ?? $users[0]->id,
-                    'level' => 4 - $block,
-                    'supervisor_id' => $supervisor?->id,
-                    'supervisor_name' => $supervisor?->employee_name,
-                ]);
-
-                $supervisors[$block] = $employee;
-            }
-
-            $admin = User::factory()->create([
-                'email' => 'admin@gmail.com',
-                'name' => 'Super Admin',
-                'password' => bcrypt('admin'),
-            ]);
 
             $allPositions = Position::all();
             foreach ($allPositions as $position) {
